b2acfe9bc748342143a3454e6f74cbb10964bad5
NikhilGaur1022 <gaur3679@gmail.com>
2025-06-28 23:19:42 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,6 @@ function App() {
           </Route>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
-          <Route path="edit-article/:id" element={<PrivateRoute><EditArticlePage /></PrivateRoute>} />
           <Route path="my-articles" element={<PrivateRoute><MyArticlesPage /></PrivateRoute>} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
